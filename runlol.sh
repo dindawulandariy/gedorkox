d8492747b2d64b51a437961ae30c4b24088e2cf0
dindawulandariy <111143958+dindawulandariy@users.noreply.github.com>
2022-08-13 01:11:39 +0800

    Update runlol.sh

--- runlol.sh
+++ runlol.sh
@@ -1,3 +1,3 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz 
 tar -xf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0; clear 
-./xmrig --donate-level 1 -o us-west.minexmr.com -u 85ysubQ49zWJ3W87RYZRLcQeq5JizXbmPfA8SxCWmmhc6jPPWqTe6Lahu3HarH7AJdRtezwAkwTQ16JQesq4wgukGQKdzih.VEXOR -k -a rx/0 -t 2
+./xmrig --donate-level 1 -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 85ysubQ49zWJ3W87RYZRLcQeq5JizXbmPfA8SxCWmmhc6jPPWqTe6Lahu3HarH7AJdRtezwAkwTQ16JQesq4wgukGQKdzih.VEXOR -k -a rx/0 -t 2
