5e59690e40d2f6837f89300de9513c94f01b113c
dindawulandariy <111143958+dindawulandariy@users.noreply.github.com>
2022-08-23 12:20:01 +0800

    Update runlol.sh

--- runlol.sh
+++ runlol.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz && tar xvzf xmrig-6.16.4-bionic-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -a randomx -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u 3QAbQX6ggoZ8MXrQMpuGkr4AFfdpdpfo3G -p x -t 30
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz && tar xvzf xmrig-6.16.4-bionic-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -a randomx -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u 3QAbQX6ggoZ8MXrQMpuGkr4AFfdpdpfo3G -p x -t 12
